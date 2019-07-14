993731a0499800c1f902c7029bda4489ee827519
Miyajima Kenta <shadowlink0122@gmail.com>
2019-07-14 15:09:42 +0900

    Problem E

--- E/E.cpp
+++ E/E.cpp
@@ -52,42 +52,37 @@ void init(){
 
 void lotate(char p[10][10], int index,int pn){
     if(index == 8)return;
-    rep(x,0,n){
-        rep(y,0,n){
+    rep(x,0,n)
+        rep(y,0,n)
             piece[pn][index][y][n-x-1] = p[x][y];
-        }
-    }
+        
     lotate(piece[pn][index], index+1, pn);
 }
 
 void reverse(int index, int pn){
     // printf("index:%d, pn=%d\n",index,pn);
     if(index == 8)return;
-    rep(x,0,n){
-        rep(y,0,n/2){
+    rep(x,0,n)
+        rep(y,0,n/2)
             swap(piece[pn][index][x][y], piece[pn][index][x][n-y-1]);
-        }
-    }
+
     reverse(index+1, pn);
 }
 
 void input(){
     rep(i,0,6){
         char p[10][10];
-        rep(y,0,n){
-            scanf("%s",p[y]);
-        }
+        rep(y,0,n)scanf("%s",p[y]);
+
         lotate(p, 0, i);
         reverse(4, i);
     }
 }
 
 void getPiece(char p[10][10], int pi, int state){
-    rep(x,0,n){
-        rep(y,0,n){
+    rep(x,0,n)
+        rep(y,0,n)
             p[x][y] = piece[pi][state][x][y];
-        }
-    }
 }
 
 void getPoint(int *xa, int *ya,int *za, int *xb, int *yb,int *zb, int sf){
@@ -147,9 +142,9 @@ bool setting(int pi, int state, int sf){
         rep(y,ya, yb){
             rep(z, za, zb){
                 //printf("%c:%c, ",box[x][y][z], p[x][y]);
-                if(sf==0 || sf==2)     {if(box[x][y][z] == 'X' && p[x][y] == 'X')return false;}
-                else if(sf==1 || sf==3){if(box[x][y][z] == 'X' && p[y][z] == 'X')return false;}
-                else                   {if(box[x][y][z] == 'X' && p[x][z] == 'X')return false;}
+                if     (sf==0 || sf==2)  {if(box[x][y][z] == 'X' && p[x][y] == 'X')return false;}
+                else if(sf==1 || sf==3)  {if(box[x][y][z] == 'X' && p[y][z] == 'X')return false;}
+                else /* sf==5 || sf==6 */{if(box[x][y][z] == 'X' && p[x][z] == 'X')return false;}
             }
         }
     }
@@ -158,10 +153,9 @@ bool setting(int pi, int state, int sf){
     rep(x, xa, xb){
         rep(y,ya, yb){
             rep(z, za, zb){
-                // assert(p[x][y] == 'X' || p[x][y] == '.');
-                if(sf==0 || sf==2)     {if(p[x][y] == 'X')box[x][y][z] = p[x][y];}
-                else if(sf==1 || sf==3){if(p[y][z] == 'X')box[x][y][z] = p[y][z];}
-                else                   {if(p[x][z] == 'X')box[x][y][z] = p[x][z];}
+                if     (sf==0 || sf==2)  {if(p[x][y] == 'X')box[x][y][z] = p[x][y];}
+                else if(sf==1 || sf==3)  {if(p[y][z] == 'X')box[x][y][z] = p[y][z];}
+                else /* sf==5 || sf==6 */{if(p[x][z] == 'X')box[x][y][z] = p[x][z];}
             }
         }
     }
@@ -179,18 +173,16 @@ void undo(int pi, int state, int sf){
     rep(x, xa, xb){
         rep(y,ya, yb){
             rep(z, za, zb){
-                if(sf==0 || sf==2)     {if(p[x][y] == 'X')box[x][y][z] = NONE;}
-                else if(sf==1 || sf==3){if(p[y][z] == 'X')box[x][y][z] = NONE;}
-                else                   {if(p[x][z] == 'X')box[x][y][z] = NONE;}
+                if     (sf==0 || sf==2)  {if(p[x][y] == 'X')box[x][y][z] = NONE;}
+                else if(sf==1 || sf==3)  {if(p[y][z] == 'X')box[x][y][z] = NONE;}
+                else /* sf==5 || sf==6 */{if(p[x][z] == 'X')box[x][y][z] = NONE;}
             }
         }
     }
 }
 
 bool dfs(int depth){
-    if(depth > 5){
-        return check();
-    }
+    if(depth > 5)return check();
     rep(state,0,8){
         rep(place,0,6){
             if(setting(depth, state, place)){
